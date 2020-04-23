fc25e6a4a404f082246c686451b5d12c045e01e3
Liliyan Krumov <liliyan.krumov@puzzel.com>
2020-04-23 18:30:21 +0300

    Home adjustments

--- src/modules/home/home.component.ts
+++ src/modules/home/home.component.ts
@@ -4,4 +4,9 @@ import { Component, OnInit } from '@angular/core';
     selector: 'xweb-home',
     templateUrl: './home.component.html'
 })
-export class HomeComponent { }
+export class HomeComponent {
+    content: Array<{ title: string, content: string }> = [
+        { title: 'Test #1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+        { title: 'Test #2', content: 'Cras interdum lacus urna, et sollicitudin odio ultricies posuere.' }
+    ];
+}
