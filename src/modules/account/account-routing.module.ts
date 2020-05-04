44fd755c0d99e995648691b14c0a9b153b696811
Liliyan Krumov <liliyan.krumov@puzzel.com>
2020-05-04 10:02:02 +0300

    Route guarding

--- src/modules/account/account-routing.module.ts
+++ src/modules/account/account-routing.module.ts
@@ -4,14 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { CharactersComponent } from './characters/characters.component'
 import { CharacterPanelComponent } from '../character-panel/character-panel.component';
 import { CharacterPanelRoutes } from '../character-panel/character-panel-routing.module';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'characters' },
-    { path: 'characters', component: CharactersComponent },
+    { path: '', redirectTo: 'characters', canActivate: [AuthGuard] },
+    { path: 'characters', component: CharactersComponent, canActivate: [AuthGuard] },
     {
         path: 'character/:name',
         component: CharacterPanelComponent,
-        children: CharacterPanelRoutes
+        children: CharacterPanelRoutes,
+        canActivate: [AuthGuard]
     }
 ];
 
