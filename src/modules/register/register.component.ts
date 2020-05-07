9349dee0be9bc50e64c482a776fab43f2b0269d4
Liliyan Krumov <liliyan.krumov@puzzel.com>
2020-05-08 00:45:53 +0300

    Form improvements

--- src/modules/register/register.component.ts
+++ src/modules/register/register.component.ts
@@ -1,10 +1,11 @@
-import { Component, AfterViewInit, ViewChild, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './register.component.html'
 })
-export class RegisterComponent implements AfterViewInit {
+export class RegisterComponent {
+    // TODO: updateOn blur?
     username = new FormControl('', [
         Validators.minLength(4),
         Validators.maxLength(10),
@@ -29,16 +30,4 @@ export class RegisterComponent implements AfterViewInit {
         Validators.required,
         Validators.email
     ]);
-
-    // TODO: Fix that
-    // matcher = new ErrorStateMatcher();
-
-    @ViewChild('usernameInput') usernameInput: ElementRef;
-
-    constructor(private changeDetector: ChangeDetectorRef) { }
-
-    ngAfterViewInit() {
-        this.usernameInput.nativeElement.focus();
-        this.changeDetector.detectChanges();
-    }
 }
--- src/modules/register/register.module.ts
+++ src/modules/register/register.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
         RegisterComponent
     ],
     imports: [
+        FormsModule,
+        ReactiveFormsModule,
         CommonModule,
         RegisterRoutingModule
     ]
